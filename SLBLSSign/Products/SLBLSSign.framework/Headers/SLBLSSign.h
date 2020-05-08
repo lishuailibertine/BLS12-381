1aa59136e9f5481cda520449a8c2adec68695693
li shuai <lishuai19@yeah.net>
2020-05-08 11:04:22 +0800

    BLS-framework

---
+++ SLBLSSign/Products/SLBLSSign.framework/Headers/SLBLSSign.h
@@ -0,0 +1,19 @@
+//
+//  SLBLSSign.h
+//  SLBLSSign
+//
+//  Created by li shuai on 2020/5/6.
+//  Copyright © 2020 li shuai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SLBLSSign.
+FOUNDATION_EXPORT double SLBLSSignVersionNumber;
+
+//! Project version string for SLBLSSign.
+FOUNDATION_EXPORT const unsigned char SLBLSSignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLBLSSign/PublicHeader.h>
+
+
